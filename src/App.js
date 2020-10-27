86d9f392feeb2bedbdf39ef11ebd339b5c22f6dc
Marjan Sadat <marjansadat10@gmail.com>
2020-10-27 15:15:53 -0700

    Competed rows

--- src/App.js
+++ src/App.js
@@ -2,19 +2,48 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
+import Banner from './Banner';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Rpmance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentarues" fetchUrl={requests.fetchDocumentries} />
+      {/* Navbar */}
+      {/* Banner */}
+
+      <Banner />
+      <Row 
+        title="NETFLIX ORIGINALS" 
+        fetchUrl={requests.fetchNetflixOriginals} 
+        isLargeRow 
+      />
+      <Row 
+        title="Trending Now" 
+        fetchUrl={requests.fetchTrending} 
+      />
+      <Row 
+        title="Top Rated" 
+        fetchUrl={requests.fetchTopRated} 
+      />
+      <Row 
+        title="Action Movies" 
+        fetchUrl={requests.fetchActionMovies} 
+      />
+      <Row 
+        title="Comedy Movies" 
+        fetchUrl={requests.fetchComedyMovies} 
+      />
+      <Row 
+        title="Horror Movies" 
+        fetchUrl={requests.fetchHorrorMovies} 
+      />
+      <Row 
+        title="Rpmance Movies" 
+        fetchUrl={requests.fetchRomanceMovies} 
+      />
+      <Row 
+        title="Documentarues" 
+        fetchUrl={requests.fetchDocumentries} 
+      />
 
     </div>
   );
